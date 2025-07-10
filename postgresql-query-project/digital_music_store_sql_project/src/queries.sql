5348d44401dd2aac061dd8793d7d202c852d1804
brndndev <brndndev@icloud.com>
2025-07-10 09:29:46 -0400

    🚀 Pushing commit: Query 2 in queries.sql

--- postgresql-query-project/digital_music_store_sql_project/src/queries.sql
+++ postgresql-query-project/digital_music_store_sql_project/src/queries.sql
@@ -1,3 +1,26 @@
 --Query 1:
 --The question I wanted to answer with this query is, "What are the Top 3 Records by highest average track length?"
 
+SELECT 
+    Album.Title AS AlbumTitle,
+    AVG(Track.Milliseconds) / 60000.0 AS AvgTrackLength_Minutes
+FROM Track
+JOIN Album ON Track.AlbumId = Album.AlbumId
+GROUP BY Album.AlbumId, Album.Title
+ORDER BY AvgTrackLength_Minutes DESC
+LIMIT 3;
+
+--Query 2:
+--The question I wanted to answer with this query is, "Who are the Lowest Performing Artists
+--by Record Sales?"
+
+SELECT 
+    Artist.Name AS ArtistName,
+    SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) AS TotalRevenue
+FROM InvoiceLine
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId
+GROUP BY Artist.ArtistId, Artist.Name
+ORDER BY TotalRevenue ASC
+LIMIT 5;
